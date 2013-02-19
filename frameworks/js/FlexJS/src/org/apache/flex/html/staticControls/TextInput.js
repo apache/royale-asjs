ee5218ef6ee091e4c1b44ac5bdf1360cb204559b
Peter Ent <pent@apache.org>
2013-02-19 21:12:14 +0000

    Added TextInput and TextArea for Javascript
    git-svn-id: https://svn.apache.org/repos/asf/flex/asjs/branches/develop@1447926 13f79535-47bb-0310-9956-ffa450edef68

---
+++ frameworks/js/FlexJS/src/org/apache/flex/html/staticControls/TextInput.js
@@ -0,0 +1,61 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+goog.provide('org.apache.flex.html.staticControls.TextInput');
+
+goog.require('org.apache.flex.core.UIBase');
+
+/**
+ * @constructor
+ * @extends {org.apache.flex.core.UIBase}
+ */
+org.apache.flex.html.staticControls.TextInput = function() {
+    org.apache.flex.core.UIBase.call(this);
+};
+goog.inherits(
+    org.apache.flex.html.staticControls.TextInput, org.apache.flex.core.UIBase
+);
+
+/**
+ * @override
+ * @this {org.apache.flex.html.staticControls.TextInput}
+ * @param {Object} p The parent element.
+ */
+org.apache.flex.html.staticControls.TextInput.prototype.addToParent = 
+    function(p) {
+    this.element = document.createElement('input');
+    this.element.setAttribute('type', 'input');
+
+    p.appendChild(this.element);
+
+    this.positioner = this.element;
+};
+
+/**
+ * @expose
+ * @this {org.apache.flex.html.staticControls.TextInput}
+ * @return {string} The text getter.
+ */
+org.apache.flex.html.staticControls.TextInput.prototype.get_text = function() {
+    return this.element.value
+};
+
+/**
+ * @expose
+ * @this {org.apache.flex.html.staticControls.TextInput}
+ * @param {string} value The text setter.
+ */
+org.apache.flex.html.staticControls.TextInput.prototype.set_text = function(value) {
+    this.element.value = value;
+};
