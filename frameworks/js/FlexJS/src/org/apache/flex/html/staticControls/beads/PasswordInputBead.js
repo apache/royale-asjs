37c681b78ab9ef9d2339d910b0cb1aabeb337b68
Peter Ent <pent@apache.org>
2013-08-09 11:49:32 -0400

    PasswordInputBead.

---
+++ frameworks/js/FlexJS/src/org/apache/flex/html/staticControls/beads/PasswordInputBead.js
@@ -0,0 +1,42 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+goog.provide('org.apache.flex.html.staticControls.beads.PasswordInputBead');
+
+
+
+/**
+ * @constructor
+ */
+org.apache.flex.html.staticControls.beads.PasswordInputBead = function() {
+
+  /**
+   * @private
+   * @type {Object}
+   */
+  this.promptElement;
+};
+
+/**
+ * @expose
+ * @this {org.apache.flex.html.staticControls.beads.PasswordInputBead}
+ * @param {Object} value The new host.
+ */
+org.apache.flex.html.staticControls.beads.PasswordInputBead.prototype.set_strand =
+    function(value) {
+  if (this.strand_ !== value) {
+    this.strand_ = value;
+    value.element.type = 'password';
+  }
+};
