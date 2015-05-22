cac7be7bf1549ba0f633186c35e5e2c23521f8cb
Alex Harui <aharui@apache.org>
2015-05-22 15:30:34 -0700

    forgot to implement JS BrowserWindow class

---
+++ frameworks/projects/Core/js/src/org/apache/flex/core/BrowserWindow.js
@@ -0,0 +1,46 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+goog.provide('org_apache_flex_core_BrowserWindow');
+
+
+
+/**
+ * @constructor
+ */
+org_apache_flex_core_BrowserWindow = function() {
+    org_apache_flex_core_BrowserWindow.base(this, 'constructor');
+  };
+
+
+/**
+ * Metadata
+ *
+ * @type {Object.<string, Array.<Object>>}
+ */
+org_apache_flex_core_BrowserWindow.prototype.FLEXJS_CLASS_INFO =
+    { names: [{ name: 'BrowserWindow',
+                qName: 'org_apache_flex_core_BrowserWindow'}]};
+
+
+/**
+ * @expose
+ * @param {string} url The url.
+ * @param {string} options The window name.
+ */
+org_apache_flex_core_BrowserWindow.open = function(url, options) {
+  Window.open(url, options);
+};
+
+
