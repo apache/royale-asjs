a02f948e4d44da7a2359a30e0a5b695ffc9a4af8
Carlos Rovira <carlosrovira@apache.org>
2018-09-09 10:46:26 +0200

    Revert "remove files in distribution bin that seems obsolete"
    This reverts commit 9680f26537bb08d2a9859b3716ff0cd1002502ac.

---
+++ distribution/src/main/resources/bin/compc.bat
@@ -0,0 +1,32 @@
+@echo off
+
+rem
+rem Licensed to the Apache Software Foundation (ASF) under one or more
+rem contributor license agreements.  See the NOTICE file distributed with
+rem this work for additional information regarding copyright ownership.
+rem The ASF licenses this file to You under the Apache License, Version 2.0
+rem (the "License"); you may not use this file except in compliance with
+rem the License.  You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+rem
+
+rem
+rem compc.bat script to launch compiler-compc.jar in Windows Command Prompt.
+rem On OSX, Unix, or Cygwin, use the compc shell script instead.
+rem
+
+setlocal
+
+if "x%ROYALE_COMPILER_HOME%"=="x"  (set ROYALE_COMPILER_HOME=%~dp0..) else echo Using Royale codebase: %ROYALE_COMPILER_HOME%
+
+if "x%ROYALE_HOME%"=="x" (set ROYALE_HOME=%~dp0..) else echo Using Royale SDK: %ROYALE_HOME%
+
+@java -Dsun.io.useCanonCaches=false -Xms32m -Xmx512m  -Droyalelib="%ROYALE_HOME%/frameworks" -jar "%ROYALE_COMPILER_HOME%\lib\compiler-compc.jar" %*
+
---
+++ distribution/src/main/resources/bin/optimizer.bat
@@ -0,0 +1,27 @@
+@echo off
+
+rem
+rem Licensed to the Apache Software Foundation (ASF) under one or more
+rem contributor license agreements.  See the NOTICE file distributed with
+rem this work for additional information regarding copyright ownership.
+rem The ASF licenses this file to You under the Apache License, Version 2.0
+rem (the "License"); you may not use this file except in compliance with
+rem the License.  You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+rem
+
+rem
+rem foptimizer.bat script to launch the compiler-optimizer.jar in a Windows
+rem Command Prompt. On OSX, Unix, or Cygwin, use the foptimizer shell script
+rem instead.
+rem
+
+@java -Dsun.io.useCanonCaches=false -Dapplication.home="%~dp0.." -Xms32m -Xmx512m -jar "%~dp0..\lib\compiler-optimizer.jar" %*
+
