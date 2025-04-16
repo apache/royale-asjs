apache/royale-asjs

commit 50d940ce9ecbf72ea628a520d08292b2c8a5f227

The reviver works bottom up so you have to copy properties when replacing a plain object with a class

frameworks/projects/Reflection/src/main/royale/org/apache/royale/reflection/beads/JSONReviver.as
@@ -108,7 +108,11 @@ public class JSONReviver
             var c:Class = classMap[key];
             if (c)
             {
-                return new c();
+                var newValue:Object = new c();
+                for (var p:String in value)
+                {
+                    newValue[p] = value[p];
+                }
             }
         }
         return value;

commit deeb1aec4557adcc7e8d343f51d013f7ed762b13

JSONReviver: when replacing a plain object with a class instance, return the class instance
Commit 50d940ce9ecbf72ea628a520d08292b2c8a5f227 accidentally missed the return statement

frameworks/projects/Reflection/src/main/royale/org/apache/royale/reflection/beads/JSONReviver.as
@@ -113,6 +113,7 @@ public class JSONReviver implements IBead
                 {
                     newValue[p] = value[p];
                 }
+                return newValue;
             }
         }
         return value;